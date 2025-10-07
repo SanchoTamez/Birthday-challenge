2c63f5a39a7a9f6094e79d8634a1acdb8ec70b50
Elan Tamez <tamezapple@gmail.com>
2025-10-07 14:43:23 -0600

    Initial Commit

---
+++ Birthday challenge/Birthday_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_challengeApp.swift
+//  Birthday challenge
+//
+//  Created by Tamez, Elan - Student on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthday challengeTests/Birthday_challengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_challengeTests.swift
+//  Birthday challengeTests
+//
+//  Created by Tamez, Elan - Student on 10/7/25.
+//
+
+import Testing
+@testable import Birthday_challenge
+
+struct Birthday_challengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
